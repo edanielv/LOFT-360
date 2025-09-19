e7c0a625882b509bff3c8823204cce4f04d89c1b
edanielv <edanielvaron.arq@gmail.com>
2025-09-19 18:08:41 -0500

    Add files via upload

---
+++ data.js
@@ -0,0 +1,175 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-recibidor-360",
+      "name": "RECIBIDOR-360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.18165556385486958,
+        "pitch": 0.09226694403461266,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.029860331233850346,
+          "pitch": 0.21345646833603737,
+          "rotation": 0,
+          "target": "1-salncocinacomedor-360"
+        },
+        {
+          "yaw": 0.5986812007193372,
+          "pitch": 0.651487165517489,
+          "rotation": 0.7853981633974483,
+          "target": "3-despacho-360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "1-salncocinacomedor-360",
+      "name": "SALÓN/COCINA/COMEDOR-360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.13828366760256117,
+        "pitch": 0.042671147299744305,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -0.6131023119819208,
+          "pitch": 0.3542050799996588,
+          "rotation": 0,
+          "target": "2-dormitorio-360"
+        },
+        {
+          "yaw": 2.801903751687232,
+          "pitch": 0.3317397060803877,
+          "rotation": 0.7853981633974483,
+          "target": "0-recibidor-360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "2-dormitorio-360",
+      "name": "DORMITORIO-360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": 0.381286477865622,
+        "pitch": 0.13357866633619864,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.758595356581637,
+          "pitch": 0.6024977277091637,
+          "rotation": 0.7853981633974483,
+          "target": "1-salncocinacomedor-360"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-despacho-360",
+      "name": "DESPACHO-360",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        },
+        {
+          "tileSize": 512,
+          "size": 2048
+        }
+      ],
+      "faceSize": 2048,
+      "initialViewParameters": {
+        "yaw": -0.206462191843519,
+        "pitch": 0.07606623622786302,
+        "fov": 1.38217411905719
+      },
+      "linkHotspots": [
+        {
+          "yaw": -1.0282561947323074,
+          "pitch": 0.46436803275963356,
+          "rotation": 5.497787143782138,
+          "target": "0-recibidor-360"
+        }
+      ],
+      "infoHotspots": []
+    }
+  ],
+  "name": "LOFT",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": false,
+    "fullscreenButton": true,
+    "viewControlButtons": true
+  }
+};
